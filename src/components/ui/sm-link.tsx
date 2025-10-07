50171946a8d4529f8014fc56ddd052da87f2746e
Jack Matthews <jackhkmatthews@gmail.com>
2025-10-07 14:29:52 +0100

    feat(homepage): add title, intro and image

---
+++ src/components/ui/sm-link.tsx
@@ -0,0 +1,52 @@
+import * as React from "react";
+import { Slot } from "@radix-ui/react-slot";
+import { cva, type VariantProps } from "class-variance-authority";
+
+import { cn } from "@/lib/utils";
+
+const linkVariants = cva(
+  "inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-[1px] font-serif underline-offset-4",
+  {
+    variants: {
+      variant: {
+        default: "text-primary hover:underline",
+        secondary:
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80",
+      },
+      size: {
+        default: "px-2 py-1 has-[>svg]:px-3",
+        sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
+        lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        icon: "size-9",
+        "icon-sm": "size-8",
+        "icon-lg": "size-10",
+      },
+    },
+    defaultVariants: {
+      variant: "default",
+      size: "default",
+    },
+  }
+);
+
+function SMLink({
+  className,
+  variant,
+  size,
+  asChild = false,
+  ...props
+}: React.ComponentProps<"a"> &
+  VariantProps<typeof linkVariants> & {
+    asChild?: boolean;
+  }) {
+  const Comp = asChild ? Slot : "a";
+
+  return (
+    <Comp
+      className={cn(linkVariants({ variant, size, className }))}
+      {...props}
+    />
+  );
+}
+
+export { SMLink, linkVariants };
---
+++ src/styles/textClasses.ts
@@ -0,0 +1,4 @@
+export const textClasses = {
+  h1: "font-serif text-2xl md:text-4xl font-bold",
+  body: "font-serif text-base",
+};
