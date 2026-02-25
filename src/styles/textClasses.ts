df7601e4c1d93e514b15503ea6233b5732bd23fd
Jack Matthews <jackhkmatthews@gmail.com>
2026-02-25 21:41:35 +0000

    feat(textClasses): update wordmark font

--- src/styles/textClasses.ts
+++ src/styles/textClasses.ts
@@ -1,7 +1,7 @@
 // Fonts change at lg breakpoint
 
 export const textClasses = {
-  wordMark: "font-wordmark text-2xl lg:text-3xl font-bold leading-none",
+  wordMark: "font-serif text-2xl lg:text-3xl font-bold leading-none",
   h1: "font-serif text-lg font-bold lg:text-2xl",
   h3: "font-serif text-base font-bold lg:text-xl",
   navLink: "font-serif text-lg lg:text-xl",
