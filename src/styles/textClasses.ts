39c67da1227e5b9136d09d76272a2d06e6e0ba92
Jack Matthews <jackhkmatthews@gmail.com>
2026-04-08 21:54:03 +0100

    feat(navLinks): shrink on mobile

--- src/styles/textClasses.ts
+++ src/styles/textClasses.ts
@@ -1,12 +1,12 @@
 // Fonts change at lg breakpoint
 
 export const textClasses = {
-  wordMark: "font-serif text-2xl lg:text-3xl font-medium leading-none",
+  wordMark: "font-serif text-base md:text-lg lg:text-3xl font-medium leading-none",
   h1: "font-serif text-lg font-bold lg:text-2xl",
   h3: "font-serif text-base font-bold lg:text-xl",
-  navLink: "font-serif text-lg lg:text-xl font-normal",
+  navLink: "font-serif text-sm md:text-base lg:text-xl font-normal",
   body: "font-serif text-base lg:text-xl",
-  portableText: "prose font-serif prose-a:font-normal max-w-[50ch]",
+  portableText: "prose font-serif prose-a:font-normal max-w-[50ch] tracking-tight",
   portableTextFooter:
     "text-xs prose-sm font-serif leading-none prose-a:font-normal",
 } as const;
